649553a77f9b610987a42e326dbf69bf93ac875f
ARMAAN199 <56539435+ARMAAN199@users.noreply.github.com>
2021-05-22 12:47:12 +0530

    delete

--- server/addons/keys.js
+++
@@ -1,6 +0,0 @@
-//add this file to .gitignore
- module.exports = {
-     google: {
-    clientID : '568208190741-qt42k5nhu0qt53m2alo93ig3mmbiugql.apps.googleusercontent.com',
-    clientSecret : 'aZ4RKI7K3YPUUsuV6yYQAJtn'
- }};
